dd613da8c498cc6d01b8ecffc32a41a4ed640afb
vi-lcandido <vitoria.lcandido@gmail.com>
2023-02-19 10:51:29 -0300

    refactor: usState to useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { Home } from "./pages/home";
-import {Login} from "./pages/login"
+import { Login } from "./pages/login";
 import { SignUp } from "./pages/sign-up/signup";
 
+function reducer(state, action) {
+  console.log(action, "action sendo disparada");
+  switch(action.type) {
+    case 'change_current_page':
+      return {
+        currentPage: action.payload
+      }
+  }
+}
+
+const initialState = {
+  currentPage: "login",
+};
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("login");
-  // console.log(currentPage);
+  const [globalState, dispatch] = useReducer(reducer, initialState);
+
+  const onClickNavigate = (page) => {
+    dispatch({ type: "change_current_page", payload: page });
+  };
+ 
   return (
     <>
-      {currentPage === "login" && <Login onClickToHomeBtn={setCurrentPage} onClickToSignUpLink={setCurrentPage} />}
-      {currentPage === "home" && <Home onClickToLoginBtn={setCurrentPage}/>}
-      {currentPage === "signup" && <SignUp backToHomeBtn={setCurrentPage}/>}
+      {globalState.currentPage === "login" && (
+        <Login
+          onClickToHomeBtn={onClickNavigate}
+          onClickToSignUpLink={onClickNavigate}
+        />
+      )}
+      {globalState.currentPage === "home" && (
+        <Home onClickToLoginBtn={onClickNavigate} />
+      )}
+      {globalState.currentPage === "signup" && (
+        <SignUp backToHomeBtn={onClickNavigate} />
+      )}
     </>
   );
 }
