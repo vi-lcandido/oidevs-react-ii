0c30a5256cbb7eb6d6016e3c7a34a8b19c12e446
vi-lcandido <vitoria.lcandido@gmail.com>
2023-02-19 20:35:23 -0300

    feat: button back to home on img page full screen

--- src/pages/photo.jsx
+++ src/pages/photo.jsx
@@ -1,11 +1,21 @@
 import { useContext } from "react";
 import { instaContext } from "../App";
+import { Button } from "../ui/button/button";
 import { Text } from "../ui/text";
 
 export const Photo = () => {
-  const { myState } = useContext(instaContext);
+  const { myState, myDispatch } = useContext(instaContext);
+
   return (
     <div>
+      <Button
+        onClick={() => {
+          myDispatch({ type: "change_current_page", payload: "home" });
+        }}
+      >
+        Voltar para Home
+      </Button>
+      <br />
       <img src={myState.user.fullPageImg.urls.regular} />
     </div>
   );
